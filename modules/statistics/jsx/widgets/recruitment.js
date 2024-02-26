48382c0eb93dd25fdf23bbfe7349ae8d130a4f3a
Saagar Arya <saagar.arya@mcin.ca>
2024-02-26 16:32:01 -0500

    Creating new dashboardeditor

--- modules/statistics/jsx/widgets/recruitment.js
+++ modules/statistics/jsx/widgets/recruitment.js
@@ -4,6 +4,7 @@ import Loader from 'Loader';
 import Panel from 'Panel';
 import {QueryChartForm} from './helpers/queryChartForm';
 import {progressBarBuilder} from './helpers/progressbarBuilder';
+import {SelectElement, FormElement, TextboxElement, TagsElement, SearchableDropdown} from 'jsx/Form';
 
 import {fetchData} from '../Fetch';
 import {setupCharts} from './helpers/chartBuilder';
@@ -19,6 +20,8 @@ const Recruitment = (props) => {
   let json = props.data;
 
   const [chartDetails, setChartDetails] = useState({});
+  const [dashboardEditorDefault, setDashboardEditorDefault] = useState({});
+  const [dashboardEditorValues, setDashboardEditorValues] = useState({})
 
   const showChart = (panelID, chartID) => {
     return props.showChart(panelID, chartID, chartDetails, setChartDetails);
@@ -31,12 +34,14 @@ const Recruitment = (props) => {
   useEffect(() => {
     if (json && Object.keys(json).length !== 0) {
       json = props.data;
-      let query = `${props.baseURL}/charts/getConfigChartNames`
-      fetchData(query).then((chartData) => {
+      let query = `${props.baseURL}/charts/getDashboardChartInfo`
+      fetchData(query).then((returnData) => {
         setLoading(false);
         // set chartDetails to have the panelIDs and chartIDs so that the divs can be created
-        setChartDetails(chartData);
-        setupCharts(false, chartData).then((data) => {
+        setChartDetails(returnData.chartData);
+
+        setDashboardConfigOptions(returnData.chartOptions);
+        setupCharts(false, returnData.chartData).then((data) => {
           // update chartDetails to have the data so that it doesn't need to be re-pulled
           setChartDetails(data);
         });
@@ -51,62 +56,124 @@ const Recruitment = (props) => {
         title='Recruitment'
         id='statistics_recruitment'
         views={[
+          // {
+          //   content:
+          //   <div className='recruitment-panel' id='overall-recruitment'>
+          //     {progressBarBuilder(json['recruitment']['overall'])}
+          //   </div>,
+          //   title: 'Recruitment - overall',
+          // },
+          // ...Object.keys(chartDetails).map((panelID) => {
+          //   return {
+          //     content:
+          //     json['recruitment']['overall']
+          //     && json['recruitment']['overall']['total_recruitment'] > 0 ?
+          //       <>
+          //         <QueryChartForm
+          //           Module={'statistics'}
+          //           name={'recruitment'}
+          //           id={'recruitment' + panelID + 'Form'}
+          //           data={json}
+          //           callback={(formDataObj) => {
+          //             updateFilters(formDataObj, panelID);
+          //           }}
+          //         />
+          //         {Object.keys(chartDetails[panelID]).map((chartID) => {
+          //           return showChart(panelID, chartID);
+          //         })}
+          //       </> :
+          //       <p>There have been no candidates registered yet.</p>,
+          //     title: 'Recruitment - ' + panelID,
+          //   }
+          // }),
+          // {
+          //   content:
+          //   <>
+          //     {Object.entries(json['recruitment']).map(([key, value]) => {
+          //       if (key !== 'overall') {
+          //         return <div key={`projectBreakdown_${key}`}>
+          //           {progressBarBuilder(value)}
+          //         </div>;
+          //       }
+          //     })}
+          //   </>,
+          //   title: 'Recruitment - project breakdown',
+          // },
+          // {
+          //   content:
+          //     <>
+          //       {Object.entries(json['recruitmentcohorts'])
+          //       .map(([key, value]) => {
+          //         return <div key={`cohortBreakdown_${key}`}>
+          //           {progressBarBuilder(value)}
+          //         </div>;
+          //         }
+          //       )}
+          //     </>,
+          //   title: 'Recruitment - cohort breakdown',
+          // },
           {
-            content:
-            <div className='recruitment-panel' id='overall-recruitment'>
-              {progressBarBuilder(json['recruitment']['overall'])}
-            </div>,
-            title: 'Recruitment - overall',
-          },
-          ...Object.keys(chartDetails).map((panelID) => {
-            return {
-              content:
-              json['recruitment']['overall']
-              && json['recruitment']['overall']['total_recruitment'] > 0 ?
-                <>
-                  <QueryChartForm
-                    Module={'statistics'}
-                    name={'recruitment'}
-                    id={'recruitment' + panelID + 'Form'}
-                    data={json}
-                    callback={(formDataObj) => {
-                      updateFilters(formDataObj, panelID);
-                    }}
-                  />
-                  {Object.keys(chartDetails[panelID]).map((chartID) => {
-                    return showChart(panelID, chartID);
-                  })}
-                </> :
-                <p>There have been no candidates registered yet.</p>,
-              title: 'Recruitment - ' + panelID,
-            }
-          }),
-          {
-            content:
-            <>
-              {Object.entries(json['recruitment']).map(([key, value]) => {
-                if (key !== 'overall') {
-                  return <div key={`projectBreakdown_${key}`}>
-                    {progressBarBuilder(value)}
-                  </div>;
+            content: <div>
+              <h2>Recruitment - edit dashboard</h2>
+              {/* move to new file */}
+              {/* need to get this to work LOL */}
+              <Panel
+                views={Object.keys(chartDetails).flatMap((panelID) => {
+                  return Object.keys(chartDetails[panelID]).map((chartID) => {
+                    return {
+                      content: <FormElement>
+                        <p>Which table should be queried?</p>
+                        <SelectElement
+                          name='queryTable'
+                          options={
+                            Object.keys(dashboadConfigOptions)
+                          } 
+                          onChangeValue={(newVal) => {
+                            let newDashboardVals = dashboardEditorValues;
+                            newDashboardVals[chartID].queryTable = newVal
+                            setDashboardEditorValues(newDashboardVals)
+                          }}
+                          value={dashboardEditorValues[chartID].queryTable}
+                          />
+                        <p>Which column is the information coming from?</p>
+                        <SelectElement
+                          name='column'
+                          options={
+                            dashboadConfigOptions[dashboardEditorValues[chartID].queryTable]
+                          } 
+                          value={dashboardEditorValues[chartID].column}
+                        />
+                        <p>What chart types?</p>
+                        <SelectElement
+                          name='chartTypes'
+                          options={['bar', 'pie']} 
+                          multiple={true}
+                        />
+                        <p>Chart Title</p>
+                        <TextboxElement
+                          name='chartTitle'
+                          placeholder='Chart Title'
+                          value={dashboardEditorValues.chartTitle}
+                        />
+                        <p>Panel Name</p>
+                        <SearchableDropdown
+                          name='panel'
+                          options={['site breakdown']}
+                        />
+                        <p>Optional WHERE clause to limit results</p>
+                        <TextboxElement 
+                          name='extraWhere'
+                        />
+                      </FormElement>,
+                      title: chartID
+                    }
+                  })
+                })
                 }
-              })}
-            </>,
-            title: 'Recruitment - project breakdown',
-          },
-          {
-            content:
-              <>
-                {Object.entries(json['recruitmentcohorts'])
-                .map(([key, value]) => {
-                  return <div key={`cohortBreakdown_${key}`}>
-                    {progressBarBuilder(value)}
-                  </div>;
-                  }
-                )}
-              </>,
-            title: 'Recruitment - cohort breakdown',
-          },
+              />
+            </div>,
+            title: 'Edit dashboard'
+          }
         ]}
       />
     </>
